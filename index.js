bfadf4f805abc5c60aaee26b54899416dee06795
David Julian <djulian@ufl.edu>
2025-06-23 11:54:45 -0400

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css'; // Optional: include if you create this file
-
-createRoot(document.getElementById('root')).render(<App />);
